59a9aec917fb29c12e36bbf7fd534e09ce1790d0
F2258487 Daniel Thomas Arcanjo Dourado <daniel.dourado@bb.com.br>
2024-06-11 12:47:49 -0300

    refactor: news system redeploy

---
+++ news-system/deployments/redeploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#delete previous replicaset
+echo "Deleting v1 data..."
+kubectl delete configmap sistema-noticias-configmap
+kubectl delete service svc-sistema-noticias
+kubectl delete pod sistema-noticias
+
+#create deployment
+echo "Applying v2 deployment"
+kubectl apply -f ./news-system.deployment.yaml
+
+#checkout news-system deployments
+echo "Checkout news system deployments"
+kubectl rollout history deployment news-system-deployment
+
+#annotate deployment
+echo "Annotating system deployment"
+kubectl annotate deployment news-system-deployment kubernetes.io/change-cause="News System v2.0.0 Deployment"
+
+echo "checkout pods, run: kubectl get pods"
+echo "checkout portal app, go to: http://{localhost_or_minikube_Internal_ip}:31750"
