0131220480b33634064532536e579a41128276d2
F2258487 Daniel Thomas Arcanjo Dourado <daniel.dourado@bb.com.br>
2024-06-11 12:15:43 -0300

    refactor: to english portal

--- news-portal/deployments/redeploy.sh
+++ news-portal/deployments/redeploy.sh
@@ -2,19 +2,19 @@
 
 #delete previous replicaset
 echo "Deleting v1 replicaset..."
-kubectl delete -f ../replicaSets/portal-noticias.replicaset.yaml
+kubectl delete -f ../replicaSets/news-portal.replicaset.yaml
 
 #create deployment
 echo "Applying v2 deployment"
-kubectl apply -f ./portal-noticias.deployment.yaml
+kubectl apply -f ./news-portal.deployment.yaml
 
 #checkout portal-noticias deployments
 echo "Checkout portal deployments"
-kubectl rollout history deployment portal-noticias-deployment
+kubectl rollout history deployment news-portal-deployment
 
 #annotate deployment
 echo "Annotating portal deployment"
-kubectl annotate deployment portal-noticias-deployment kubernetes.io/change-cause="News Portal v2.0.0 Deployment"
+kubectl annotate deployment news-portal-deployment kubernetes.io/change-cause="News Portal v2.0.0 Deployment"
 
 echo "checkout pods, run: kubectl get pods"
 echo "checkout portal app, go to: http://{localhost_or_minikube_Internal_ip}:31500"
